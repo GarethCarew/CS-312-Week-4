b62be3713cab7f3fc0afc6bda13e91e1863dc3d6
Gareth Carew <50182665+GarethCarew@users.noreply.github.com>
2024-06-29 16:07:46 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser");
-const { it } = require("node:test");
+const mongoose = require('mongoose');
 
 const app = express()
 
@@ -9,40 +9,92 @@ app.use(express.static("public"))
 
 app.set('view engine', 'ejs');
 
-let items = []
-let workItems = []
+mongoose.connect("mongodb+srv://gmc327:XkbeIuWKD0rQkN9J@cluster0.aufu7kz.mongodb.net/todolistDB?retryWrites=true&w=majority&appName=Cluster0")
+
+const itemsSchema = {
+    name: String
+}
+
+const Item = mongoose.model("Item", itemsSchema)
+
+const item1 = new Item({name: "Welcome to your todo list"})
+
+const item2 = new Item({name: "Hit the + to add new items"})
+
+const item3 = new Item({name: "<- click to delete"})
+
+const defaultItems = [item1, item2, item3]
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+}
+
+const List = mongoose.model("List", listSchema)
 
 app.get("/", (req, res) => {
 
-    let today = new Date()
+        Item.find({}).then((foundItems) => {
 
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
+            if(foundItems.length === 0){
+                Item.insertMany(defaultItems).then((items) => {
+                    console.log(items)
+                })
+        
+                res.redirect("/")
+            } else {
+                res.render("list", {listTitle: "Today", newListItems: foundItems})
+            }
 
-    let day = today.toLocaleString("en-US", options)
-
-    res.render("list", {listTitle: day, newListItems: items})
+        })
 })
 
-app.get("/work", (req, res) => {
+app.get("/:customListName", (req, res) => {
+    
+    const customListName = req.params.customListName
 
-    res.render("list", {listTitle: "Work List", newListItems: workItems})
+    List.findOne({name: customListName}).then((foundList) => {
+        if(!foundList)
+        {
+            const list = new List({
+                name: customListName,
+                items: defaultItems
+            })
+        
+            list.save()
+            res.redirect("/" + customListName)
+        } else {
+            res.render("list", {listTitle: foundList.name, newListItems: foundList.items})
+        }
+    })
+
 })
 
 app.post("/", (req, res) => {
 
-    item = req.body.newItem
-    if(req.body.list === "Work List"){
-        workItems.push(item)
-        res.redirect("/work")
-    } else {
-        items.push(item)
-        res.redirect("/")
-    }
+    const itemName = req.body.newItem
+
+    const newItem = new Item({name: itemName})
+
+    newItem.save()
+
+    res.redirect("/")
+    
 })
+
+app.post("/delete", (req, res) => {
+
+    const checkedItemId = req.body.checkbox
+
+    Item.findByIdAndDelete(checkedItemId).exec().then((res) => {
+
+    })
+
+    res.redirect("/")
+
+})
+
+
 
 app.listen(3000, () => {
     console.log("server is running")
